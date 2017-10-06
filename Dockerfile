e639bc88eb1c230703aed57507b70c96f706f5d1
Ricardo Job <sousajob@gmail.com>
2017-10-06 11:08:37 -0300

    Extraindo as informações dos vocabulários de Software

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM java:openjdk-8-jre-alpine
 COPY run.sh run.sh
+RUN apk update
+RUN apk add ca-certificates wget
+RUN update-ca-certificates 
+RUN wget http://users.encs.concordia.ca/~nikolaos/files/pattern_detection/pattern4.jar
+# podemos substituir esse download por arquivos locais
+RUN wget http://www.softwarevocabulary.org/vocabulary-tool/download/TermsCounter.jar
+RUN wget http://www.softwarevocabulary.org/vocabulary-tool/download/VocabularyExtractor.jar
+RUN wget http://www.softwarevocabulary.org/vocabulary-tool/download/termsCounter.properties -O termsCounter.properties
+#COPY termsCounter_file.properties termsCounter.properties
 RUN ["chmod", "+x", "run.sh"]
 VOLUME ["/output"]
 ENTRYPOINT [ "sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # recebendo o nome do projeto por parametro, podemos usar um arquivo com todos os links e processa-lo
 echo $1
-wget http://users.encs.concordia.ca/~nikolaos/files/pattern_detection/pattern4.jar
+ls
 wget http://java.labsoft.dcc.ufmg.br/qualitas.class/corpus/$1.zip
 unzip -q $1.zip
 java -Xms64m -Xmx2048m -jar pattern4.jar -target $1/bin -output output/$1.xml
+#java -jar VocabularyExtractor.jar -n "axion-1.0-M2" -r "axion-1.0-M2" -d "./src/" -loc iah -vxl "./output/axion-1.0-M2.vxl" -csv "./output/axion-1.0-M2.csv"
+#java -jar TermsCounter.jar -prop ./termsCounter.properties -vxl ./output/axion-1.0-M2.vxl -csv ./output/axion-1.0-M2_Matriz.csv -txt ./output/axion-1.0-M2_Info.txt
+java -jar VocabularyExtractor.jar -n "$1" -r "$1" -d "$1/src/" -loc iah -vxl "output/$1.vxl" -csv "output/$1.csv"
+java -jar TermsCounter.jar -prop termsCounter.properties -vxl ./output/$1.vxl -csv output/$1_Matriz.csv -txt output/$1_Info.txt
