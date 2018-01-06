2fe3ce39886e8fa209db2bea9bdffe06b030de8e
natansevero <natanseverolp@gmail.com>
2018-01-06 15:30:41 -0200

    criando model da extração de vocabulario

---
+++ extractf/src/main/java/br/edu/ifpb/gpes/extractf/models/VocabularyExtractor.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.gpes.extractf.models;
+
+/**
+ *
+ * @author natan
+ */
+public class VocabularyExtractor {
+    
+    private String entityName;
+    private int LOCCount;
+    private int headerCount;
+    private int annotationCount;
+    private int innerEntitiesLines;
+    private int total;
+    private String entityType;
+
+    public VocabularyExtractor() {
+    
+    }
+    
+    public VocabularyExtractor(String entityName, int LOCCount, int headerCount, int annotationCount, int innerEntitiesLines, int total, String entityType) {
+        this.entityName = entityName;
+        this.LOCCount = LOCCount;
+        this.headerCount = headerCount;
+        this.annotationCount = annotationCount;
+        this.innerEntitiesLines = innerEntitiesLines;
+        this.total = total;
+        this.entityType = entityType;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public int getLOCCount() {
+        return LOCCount;
+    }
+
+    public void setLOCCount(int LOCCount) {
+        this.LOCCount = LOCCount;
+    }
+
+    public int getHeaderCount() {
+        return headerCount;
+    }
+
+    public void setHeaderCount(int headerCount) {
+        this.headerCount = headerCount;
+    }
+
+    public int getAnnotationCount() {
+        return annotationCount;
+    }
+
+    public void setAnnotationCount(int annotationCount) {
+        this.annotationCount = annotationCount;
+    }
+
+    public int getInnerEntitiesLines() {
+        return innerEntitiesLines;
+    }
+
+    public void setInnerEntitiesLines(int innerEntitiesLines) {
+        this.innerEntitiesLines = innerEntitiesLines;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(String entityType) {
+        this.entityType = entityType;
+    }
+
+    @Override
+    public String toString() {
+        return "VocabularyExtractor{" + "entityName=" + entityName + ", LOCCount=" + LOCCount + ", headerCount=" + headerCount + ", annotationCount=" + annotationCount + ", innerEntitiesLines=" + innerEntitiesLines + ", total=" + total + ", entityType=" + entityType + '}';
+    }
+    
+    
+}
