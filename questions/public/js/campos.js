72f9a4e71c49d2f1e056c3fa65716dca325c82f0
natansevero <natanseverolp@gmail.com>
2018-07-16 16:08:07 -0300

    Ajustando movimentação dos cards do questionario

--- questions/public/js/campos.js
+++ questions/public/js/campos.js
@@ -1,3 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+});
+
+// Array com as 10 questões
+let questoes = document.getElementsByClassName('questao')
+let indexQuestion = 0;
+questoes[indexQuestion].classList.remove('hide')
+
+function proximo() {
+    console.log("pro")
+    if(indexQuestion == 9) {
+        // submit
+    } else {
+        questoes[indexQuestion].classList.add('hide')        
+        ++indexQuestion;
+        questoes[indexQuestion].classList.remove('hide')
+        questoes[indexQuestion].classList.add('fadeInRightBig')
+        
+    }
+}
+
+function anterior() {
+    console.log("anterior")
+    if(indexQuestion == 0) {
+        // do something
+    } else {
+        questoes[indexQuestion].classList.add('hide')
+        --indexQuestion;
+        questoes[indexQuestion].classList.remove('fadeInRightBig')        
+        questoes[indexQuestion].classList.remove('hide')
+        questoes[indexQuestion].classList.add('fadeInLeftBig')        
+    }
+}
+
 function addCampoSuguestao() {
     let campos = document.getElementsByName('suguestao[nome]');
     let ultimoCampoDeTermos = campos[campos.length - 1];
