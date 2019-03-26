e63cbb23f160f557cd24755003c7c48ab8a6f5e3
natansevero <natanseverolp@gmail.com>
2019-03-26 07:02:43 -0300

    Visualizando dados do benchmarking no modulo frontend

--- frontend/src/pages/Benchmarking/index.js
+++ frontend/src/pages/Benchmarking/index.js
@@ -9,7 +9,8 @@ import { listProjectNames, listBenchmarkingByProjectName } from '../../requests'
 export default class BenchmarkingPage extends Component {
     state = {
         projectName: '',
-        options: []
+        options: [],
+        patterns: []
     }
 
     componentDidMount() {
@@ -30,7 +31,67 @@ export default class BenchmarkingPage extends Component {
         e.preventDefault();
 
         let data = await listBenchmarkingByProjectName(this.state.projectName);
-        console.log(data);
+
+        let patterns = data.patterns;
+        let terms = data.terms;
+
+        let newPatterns =
+            patterns
+                .filter(pattern => pattern.instances.length > 0)
+                .map(pattern => {
+                    let { instances } = pattern;
+
+                    for (let i = 0; i < instances.length; i++) {
+                        let { roles } = instances[i];
+
+                        for (let k = 0; k < roles.length; k++) {
+                            let { element } = roles[k];
+
+                            let newTerms = [];
+
+                            for (let j = 0; j < terms.length; j++) {
+                                let { entityName } = terms[j];
+
+                                let keys;
+
+                                if (element === entityName) {
+                                    let { termsWithCounter } = terms[j];
+                                    keys = Object.keys(termsWithCounter)
+
+                                    for (let h = 0; h < keys.length; h++) {
+                                        newTerms.push({ term: keys[h], value: termsWithCounter[keys[h]] })
+                                    }
+                                }
+                            }
+
+                            roles[k].terms = newTerms;
+                        }
+
+                        instances[i].roles = roles;
+                    }
+
+                    pattern.instances = instances;
+                    return pattern;
+                });
+
+
+        this.setState({ ...this.state, patterns: newPatterns })
+        console.log(newPatterns)
+    }
+
+    headerTemplate = roles => {
+        return (
+            <>
+                {roles.map((role, roleIndex) => (
+                    <div key={roleIndex}>
+                        <span>
+                            <b>{role.name}</b>: {role.element}
+                        </span>
+                        <br />
+                    </div>
+                ))}
+            </>
+        )
     }
 
     render() {
@@ -39,33 +100,39 @@ export default class BenchmarkingPage extends Component {
                 <PageTitle
                     title='Benchmarking'
                     subtitle='Visualização  dos dados da ferramenta de detecção de padrões de projeto e extração de vocabulário de cada projeto' />
-                
-                <ProjectSelection 
+
+                <ProjectSelection
                     projectName={this.state.projectName}
-                    options={this.state.options} 
-                    handleSelectChange={this.handleSelectChange} 
-                    handleSubmit={this.handleSubmit}  />
+                    options={this.state.options}
+                    handleSelectChange={this.handleSelectChange}
+                    handleSubmit={this.handleSubmit} />
 
                 <Accordion multiple={true}>
-                    <AccordionTab header='Design Pattern Name'>
-                        <Accordion multiple={true}>
-                            <AccordionTab header='Instance 1 (Entity Role 1, Entity Role 2)'>
-                                <h4>Entity Terms</h4>
-                                <Row>
-                                    {new Array(30).fill(5).map((item, index) => (
-                                        <Col key={index} md={2}>Term {index + 1}</Col>
-                                    ))}
-                                </Row>
+                    {
+                        this.state.patterns.map((pattern, patternIndex) => (
+                            <AccordionTab key={patternIndex} header={pattern.patternName}>
+                                <Accordion multiple={true}>
+                                    {
+                                        pattern.instances.map((instance, instanceIndex) =>
+                                            <AccordionTab key={instanceIndex} header={this.headerTemplate(instance.roles)}>
+                                                <h4>Entity Terms</h4>
+                                                <Row>
+                                                    {instance.roles.map(role => (
+                                                        role.terms.map((termObject, index) => (
+                                                            <Col key={index} md={3}>
+                                                                {termObject.term}: {termObject.value}
+                                                            </Col>
+                                                        ))
+                                                    ))}
+                                                </Row>
+                                            </AccordionTab>
+                                        )
+                                    }
+                                </Accordion>
                             </AccordionTab>
-                            <AccordionTab header='Instance 2 (Entity Role 1, Entity Role 2)'>
-                                <Row>
-                                    {new Array(30).fill(6).map((item, index) => (
-                                        <Col key={index} md={2}>Term {index + 1}</Col>
-                                    ))}
-                                </Row>
-                            </AccordionTab>
-                        </Accordion>
-                    </AccordionTab>
+                        ))
+                    }
+
                 </Accordion>
             </>
         )
