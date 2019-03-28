47a323231940ca4379112f527b19cced2e81bc2b
natansevero <natanseverolp@gmail.com>
2019-03-28 07:03:04 -0300

    Renomeando arquivos de download e execução das ferramentas

---
+++ download-tools.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget http://users.encs.concordia.ca/~nikolaos/files/pattern_detection/pattern4.jar
+# podemos substituir esse download por arquivos locais
+wget http://www.softwarevocabulary.org/vocabulary-tool/download/TermsCounter.jar
+wget http://www.softwarevocabulary.org/vocabulary-tool/download/VocabularyExtractor.jar
+wget http://www.softwarevocabulary.org/vocabulary-tool/download/termsCounter.properties -O termsCounter.properties
