1d836b87feb0c88a3d45dae9be37cb1df60ceb72
natansevero <natanseverolp@gmail.com>
2019-05-01 09:22:55 -0300

    Gerando arquivo principal com todos os dados do questionário

--- questions/scripts/generateMainFile.js
+++ questions/scripts/generateMainFile.js
@@ -8,3 +8,21 @@ let usersThatFinishedRawData = fs.readFileSync(path.join(__dirname, '..', '..', 
 let users = JSON.parse(usersRawData);
 let newCalcs = JSON.parse(newCalcsRawData);
 let usernamesFinished = JSON.parse(usersThatFinishedRawData);
+
+let mainDatas = usernamesFinished.map(username => {
+    let user = users
+        .filter(u => u.resposta.length > 0)
+        .find(u => u.username === username);
+    
+    let calc = newCalcs
+        .find(c => c.usuario === username);
+
+    return {
+        usuario: username,
+        resposta: user.resposta,
+        suguestao: user.suguestao,
+        exp: calc.exp
+    }
+})
+
+fs.writeFileSync(path.join(__dirname, '..', '..', 'questions-data', `mainDatas.json`), JSON.stringify(mainDatas));
