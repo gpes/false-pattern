dcee6fe5bd9026f37ec1accfd84c525cf1b6271b
natansevero <natanseverolp@gmail.com>
2019-05-28 07:02:13 -0300

    Iniciando dev do frontend da aplicação de classificação

--- classifications/controllers/indicios.js
+++ classifications/controllers/indicios.js
@@ -1,13 +1,19 @@
+const axios = require('axios');
+
 module.exports = app => {
     const indiciosService = app.services.indicios;
 
     return {
         render: async (req, res) => {
             try {
-                let { rows } = await indiciosService.findAll();
-                console.log('Indicios:', rows);
+                // let { rows } = await indiciosService.findAll();
 
-                res.render('index');
+                let axiosResponse = 
+                    await axios.get('https://raw.githubusercontent.com/gpes/false-pattern/master/fp-detection/src/main/java/br/edu/ifpb/gpes/fp/detection/AppFalsePatternFileGenerator.java')
+
+                res.render('index', {
+                    code: axiosResponse.data
+                });
             } catch(e) {
                 res.redirect('/error')
                 console.log(e.message)
