1f15c2f40048f8730fe49f626b60596b1c5a5558
natansevero <natanseverolp@gmail.com>
2019-06-09 15:43:30 -0300

    Ajustes no frontend e quantidade de classificação máxima

--- classifications/controllers/respostas.js
+++ classifications/controllers/respostas.js
@@ -5,7 +5,7 @@ module.exports = app => {
         answer: async (req, res) => {
             let data = await respostasService.answer(req.session.indicios[req.session.current_position].id, req.body.answer);
             
-            if(req.session.current_position === 9) {
+            if(req.session.current_position === (req.session.indicios.length - 1)) {
                 res.redirect('/finished')
             } else {
                 ++req.session.current_position;
--- classifications/services/indicios.js
+++ classifications/services/indicios.js
@@ -4,22 +4,21 @@ module.exports = app => {
     return {
         randomIndicios: async () => {
             let { rows } = await indiciosRepository.findAll();
-            let indicios = _getRandom(rows, 10);
-            return indicios;
+            return rows;
         },
     }
 }
 
-const _getRandom = (arr, n) => {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-    if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
-    }
-    return result;
-}
+// const _getRandom = (arr, n) => {
+//     var result = new Array(n),
+//         len = arr.length,
+//         taken = new Array(len);
+//     if (n > len)
+//         throw new RangeError("getRandom: more elements taken than available");
+//     while (n--) {
+//         var x = Math.floor(Math.random() * len);
+//         result[n] = arr[x in taken ? taken[x] : x];
+//         taken[x] = --len in taken ? taken[len] : len;
+//     }
+//     return result;
+// }
