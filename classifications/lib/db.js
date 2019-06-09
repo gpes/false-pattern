be8df8f4fd62ff8497be621d10ceba5ec812ae42
natansevero <natanseverolp@gmail.com>
2019-06-09 11:39:04 -0300

    Ajustando node envs da aplicação

--- classifications/lib/db.js
+++ classifications/lib/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
-// const connString = 'postgres://postgres:12345@localhost:5432/classification';
-const connString = "postgres://postgres:12345@classification-db:5432/classification";
-// const connString = process.env.DATABASE_URL;
-
-let single_connection;
 
 module.exports = app => {
+    let connString;
+
+    if(process.env.NODE_ENV == 'production') {
+        connString = process.env.DATABASE_URL;
+    } else if(process.env.NODE_ENV === 'docker') {
+        connString = 'postgres://postgres:12345@classification-db:5432/classification';
+    } else {
+        connString = 'postgres://postgres:12345@localhost:5432/classification';
+    } 
+    
+    let single_connection;
+
     if(!single_connection){
         const pool = new Pool({
             connectionString: connString
