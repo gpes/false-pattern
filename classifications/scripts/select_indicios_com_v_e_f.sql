18252d0c8f0680329c50c8f8d7410ba9b298676a
natansevero <natanseverolp@gmail.com>
2019-06-13 07:04:29 -0300

    Sql para trazer todos os indicios que foram classificados com V e F

---
+++ classifications/scripts/select_indicios_com_v_e_f.sql
@@ -0,0 +1,11 @@
+﻿select 
+* 
+from indicio i
+where 
+(
+	select count(*) from resposta where id_indicio = i.id and resposta = true
+) = 1
+and
+(
+	select count(*) from resposta where id_indicio = i.id and resposta = false
+) = 1
