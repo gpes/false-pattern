a4a80021a035b1b2d0395ba8c5366255d51d1079
natansevero <natanseverolp@gmail.com>
2019-06-17 07:04:06 -0300

    Gerando CSV das respostas das classificações

--- classifications/scripts/select_indicios_com_f_f.sql
+++ classifications/scripts/select_indicios_com_f_f.sql
@@ -5,3 +5,6 @@ where
 (
 	select count(*) from resposta where id_indicio = i.id and resposta = false
 ) = 2;
+
+--CSV
+\copy (select i.projeto, i.entidade, i.metrica, (select resposta from resposta where id_indicio = i.id limit 1) as resposta from indicio i where (select count(*) from resposta where id_indicio = i.id and resposta = false) = 2) to './../classifications-data/false-answers.csv' with csv header;
--- classifications/scripts/select_indicios_com_v_e_f.sql
+++ classifications/scripts/select_indicios_com_v_e_f.sql
@@ -9,3 +9,5 @@ and
 (
 	select count(*) from resposta where id_indicio = i.id and resposta = false
 ) = 1;
+
+\copy (select i.projeto, i.entidade, i.link, i.metrica from indicio i where (select count(*) from resposta where id_indicio = i.id and resposta = true) = 1 and (select count(*) from resposta where id_indicio = i.id and resposta = false) = 1) to './../classifications-data/indicios-v-f.csv' with csv header;
